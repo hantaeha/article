324d7ca0d0643957cd87d563468f1af8bd1ed044
hantaeha <45843679+hantaeha@users.noreply.github.com>
2019-07-17 15:21:27 +0900

    Update test_a.php

--- test_a.php
+++ test_a.php
@@ -1,5 +1,3 @@
-<form action="test_b.php" method="post">
-  ▶ 아이디 <input type="text" name="test_id">
- ▶ 이름 <input type="text" name="test_name">
- <input type="submit" value="POST로 제출">
-</form>
+<?php
+file_put_contents('data/'.$_POST['title'], $_POST['description']);
+?>
